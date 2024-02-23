fd6742dcd374d8eb0da3d4824a3885f1d314f4ab
Nati158 <141584277+Nati158@users.noreply.github.com>
2024-02-23 09:08:48 +0100

    Add files via upload

---
+++ main31.py
@@ -0,0 +1,9 @@
+def calculate_average(num1, num2, num3):
+    return (num1 + num2 + num3) / 3
+
+if __name__ == "__main__":
+    num1 = 10
+    num2 = 20
+    num3 = 30
+    average = calculate_average(num1, num2, num3)
+    print("The average is:", average)
